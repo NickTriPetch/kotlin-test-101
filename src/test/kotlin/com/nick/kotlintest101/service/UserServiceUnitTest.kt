b1098fa770e27f35cbdf6e2c98e443fedbec83bf
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2022-11-15 23:21:26 +0700

    chore: refactor codes

--- src/test/kotlin/com/nick/kotlintest101/service/UserServiceUnitTest.kt
+++ src/test/kotlin/com/nick/kotlintest101/service/UserServiceUnitTest.kt
@@ -3,24 +3,19 @@ package com.nick.kotlintest101.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.nick.kotlintest101.entity.User
 import com.nick.kotlintest101.repository.UserRepository
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
-import org.mockito.Captor
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
 import org.mockito.ArgumentMatchers.any
 import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.MockBean
 
+@ExtendWith(MockitoExtension::class)
 class UserServiceUnitTest {
-    @Autowired
+    @Mock
     lateinit var mockUserRepository: UserRepository
 
     @InjectMocks
@@ -28,15 +23,15 @@ class UserServiceUnitTest {
 
     @BeforeEach
     fun setUp() {
-        mockUserRepository = Mockito.mock(UserRepository::class.java)
+//        mockUserRepository = Mockito.mock(UserRepository::class.java)
 
-        Mockito.`when`(mockUserRepository.save(any(User::class.java))).thenReturn(User("No care", "no@care.com"))
-
-        userService = UserService(mockUserRepository)
+//        userService = UserService(mockUserRepository)
     }
 
     @Test
     fun `userRepositorySave should be called once`() {
+        Mockito.`when`(mockUserRepository.save(any(User::class.java))).thenReturn(User("No care", "no@care.com"))
+
         userService.save(
             "whatever title",
             "whatever@email.com",
@@ -46,6 +41,10 @@ class UserServiceUnitTest {
 
         Mockito.verify(mockUserRepository).save(userArgumentCaptor.capture())
         val capturedUser = userArgumentCaptor.value
+
+        println("============================================")
+        println("> capturedUser")
+        println(ObjectMapper().writeValueAsString(capturedUser))
 
         Mockito.verify(mockUserRepository, Mockito.times(1)).save(capturedUser)
     }
